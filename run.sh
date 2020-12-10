2af73d1c3ad3270a0f6f3f5e854b2f7e60266982
sima-r <sima.rahimi@skane.se>
2020-12-10 16:24:01 +0100

    add test run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+module  load  Java  singularity
+fluffy  --sample /fs1/sima/nipt/samplesheet/samplesheet2.csv --project /fs1/sima/nipt/201109_NB501697_0219_AHGK7TAFX2/test_data  --out /fs1/sima/nipt/result  --config  /fs1/sima/nipt/Config.cmd.json  analyse
